7f943bb1e79b064c9c0e463e22b021d8d832dd2b
gouz7514 <gouz7514@gmail.com>
2021-05-05 18:45:07 +0900

    update redux 배워보기 : state, reducer, action, subscribe

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import {createStore} from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+// 이런 방식으로 오류 방지 가능
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => {
+  countStore.dispatch({type:ADD});
+})
+minus.addEventListener("click", () => {
+  countStore.dispatch({type:MINUS});
+})
