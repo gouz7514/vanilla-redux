af3e0f8f5b96619ac6231b7c600c491f160cc124
gouz7514 <gouz7514@gmail.com>
2021-05-10 16:55:50 +0900

    mapDispatchToProps 사용해보기

--- src/store.js
+++ src/store.js
@@ -4,14 +4,14 @@ import {createStore} from "redux";
 const ADD = "ADD";
 const DELETE = "DELETE";
 
-export const addToDo = (text) => {
+const addToDo = (text) => {
   return {
     type:ADD,
     text
   };
 };
 
-export const deleteToDo = id => {
+const deleteToDo = id => {
   return {
     type:DELETE,
     id
@@ -30,5 +30,10 @@ const reducer = (state = [], action) => {
 }
 
 const store = createStore(reducer);
+
+export const actionCreatros = {
+  addToDo,
+  deleteToDo
+};
 
 export default store;
