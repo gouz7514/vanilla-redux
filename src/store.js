c9d3eb579f117d7c2d03ab13a23851ec7677a73b
gouz7514 <gouz7514@gmail.com>
2021-05-13 17:00:20 +0900

    redux/toolkit : createReducer 연습

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import {createStore} from "redux";
-import {createAction} from "@reduxjs/toolkit";
+import {createAction, createReducer} from "@reduxjs/toolkit";
 
 // const ADD = "ADD";
 // const DELETE = "DELETE";
@@ -21,17 +21,25 @@ const deleteToDo = createAction("DELETE");
 //   };
 // };
 
-const reducer = (state = [], action) => {
-  switch(action.type) {
-    case addToDo.type:
-      console.log(action);
-      return [{text: action.payload, id:Date.now()}, ...state];
-    case deleteToDo.type:
-      return state.filter(toDo => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-}
+// const reducer = (state = [], action) => {
+//   switch(action.type) {
+//     case addToDo.type:
+//       console.log(action);
+//       return [{text: action.payload, id:Date.now()}, ...state];
+//     case deleteToDo.type:
+//       return state.filter(toDo => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// }
+
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({text: action.payload, id:Date.now()});
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter(toDo => toDo.id !== action.payload)
+})
 
 const store = createStore(reducer);
 
